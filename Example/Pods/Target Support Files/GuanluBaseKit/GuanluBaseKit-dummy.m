745cde8439cfe0a08ee70ccec4385cfe28d91959
jiang_xg_14@163.com <371326ab>
2021-05-29 18:16:40 +0800

    提交

---
+++ Example/Pods/Target Support Files/GuanluBaseKit/GuanluBaseKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GuanluBaseKit : NSObject
+@end
+@implementation PodsDummy_GuanluBaseKit
+@end
---
+++ Example/Pods/Target Support Files/Pods-GuanluBaseKit_Example/Pods-GuanluBaseKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GuanluBaseKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_GuanluBaseKit_Example
+@end
